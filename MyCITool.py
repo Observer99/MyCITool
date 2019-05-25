97fda39d977c3edaba705390a8eac3f2ca4d1200
Michael Haskelsky <mvh1976@gmail.com>
2019-05-25 07:52:47 -0400

    Adding MyCITool.py

---
+++ MyCITool.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import sys
+import docker
+
+path_to_docker_default = "https://github.com/Observer99/MyCITool.git#master:dockerWebApp"
+image_name_default = "michaelh/my-hello"
+container_name_default = "my-hello-1"
+container_port_default = 8080
+host_port_default = 4001
+
+client = docker.from_env()
+
+#Get string parameters from user or set default values:
+path_to_docker = input('{} {} \n'.format('Please insert path to Github project that contains docker application or press Enter for default value\n\
+DEFAULT value is', path_to_docker_default)) or path_to_docker_default
+print('===>\n', 'path_to_docker set to: ', path_to_docker, '\n<===')
+
+image_name = input('{} {} \n'.format('Please insert image name or press Enter for default value\n\
+DEFAULT value is', image_name_default)) or image_name_default
+print('===>\n', 'image_name set to: ', image_name, '\n<===')
+
+container_name = input('{} {} \n'.format('Please insert container name or press Enter for default value\n\
+DEFAULT value is', container_name_default)) or container_name_default
+print('===>\n', 'container_name set to: ', container_name, '\n<===')
+
+#Get and verify integer parameters from user or set default values:
+def set_container_port():
+    container_port = input('{} {} \n'.format('Please insert container port number or press Enter for default value\nDEFAULT value is', container_port_default)) or container_port_default
+    while True:
+        try:
+    	    container_port = int(container_port)
+        except ValueError:
+            container_port = input('Host port number should be an integer! You can press Enter for default value\n') or container_port_default
+            continue
+        else:
+            break
+    return container_port
+
+container_port = set_container_port()
+print('===>\n', 'container_port set to: ', container_port, '\n<===')
+
+def set_host_port():
+    host_port = input('{} {} \n'.format('Please insert host port number (integer) or press Enter for default value\nDEFAULT value is', host_port_default)) or host_port_default
+    while True:
+        try:
+    	    host_port = int(host_port)
+        except ValueError:
+            host_port = input('Host port number should be an integer! You can press Enter for default value\n') or host_port_default
+            continue
+        else:
+            break
+    return host_port
+
+host_port = set_host_port()
+print('===>\n', 'host_port set to: ', host_port, '\n<===')
+
+#Build docker image:
+try:
+    client.images.build(path = path_to_docker, tag = image_name)
+except as e:
+    print('ERROR! The image was not built successfully!')
+    print(e)
+	exit(1)
+else:
+    print('SUCCESS! The image was built successfully!')
+
+#Run container
+try:
+    container = client.containers.run(image_name, ports = {container_port: host_port}, name = container_name, detach=True)
+except as e:
+    print('ERROR! The container was not run successfully!')
+    print(e)
+    exit(1)
+
