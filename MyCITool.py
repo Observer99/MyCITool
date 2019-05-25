63ab55b7546284fd500be23f1563ae112ce7006e
Michael Haskelsky <mvh1976@gmail.com>
2019-05-25 11:52:26 -0400

    All features complited in MyCITool.py

--- MyCITool.py
+++ MyCITool.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import docker
+from shutil import copyfile
+from time import sleep
 
 path_to_docker_default = "https://github.com/Observer99/MyCITool.git#master:dockerWebApp"
 image_name_default = "michaelh/my-hello"
@@ -14,62 +17,91 @@ client = docker.from_env()
 #Get string parameters from user or set default values:
 path_to_docker = input('{} {} \n'.format('Please insert path to Github project that contains docker application or press Enter for default value\n\
 DEFAULT value is', path_to_docker_default)) or path_to_docker_default
-print('===>\n', 'path_to_docker set to: ', path_to_docker, '\n<===')
+print('===>\n', 'path_to_docker set to: ', path_to_docker, '\n<===\n')
 
 image_name = input('{} {} \n'.format('Please insert image name or press Enter for default value\n\
 DEFAULT value is', image_name_default)) or image_name_default
-print('===>\n', 'image_name set to: ', image_name, '\n<===')
+print('===>\n', 'image_name set to: ', image_name, '\n<===\n')
 
 container_name = input('{} {} \n'.format('Please insert container name or press Enter for default value\n\
 DEFAULT value is', container_name_default)) or container_name_default
-print('===>\n', 'container_name set to: ', container_name, '\n<===')
+print('===>\n', 'container_name set to: ', container_name, '\n<===\n')
 
 #Get and verify integer parameters from user or set default values:
 def set_container_port():
-    container_port = input('{} {} \n'.format('Please insert container port number or press Enter for default value\nDEFAULT value is', container_port_default)) or container_port_default
+    container_port = input('{} {} \n'.format('Please insert container port or press Enter for default value\nDEFAULT value is', container_port_default)) or container_port_default
     while True:
         try:
     	    container_port = int(container_port)
         except ValueError:
-            container_port = input('Host port number should be an integer! You can press Enter for default value\n') or container_port_default
+            container_port = input('Container port should be an integer! You can press Enter for default value\n') or container_port_default
             continue
         else:
             break
     return container_port
 
 container_port = set_container_port()
-print('===>\n', 'container_port set to: ', container_port, '\n<===')
+print('===>\n', 'container_port set to: ', container_port, '\n<===\n')
 
 def set_host_port():
-    host_port = input('{} {} \n'.format('Please insert host port number (integer) or press Enter for default value\nDEFAULT value is', host_port_default)) or host_port_default
+    host_port = input('{} {} \n'.format('Please insert host port (integer) or press Enter for default value\nDEFAULT value is', host_port_default)) or host_port_default
     while True:
         try:
     	    host_port = int(host_port)
         except ValueError:
-            host_port = input('Host port number should be an integer! You can press Enter for default value\n') or host_port_default
+            host_port = input('Host port should be an integer! You can press Enter for default value\n') or host_port_default
             continue
         else:
             break
     return host_port
 
 host_port = set_host_port()
-print('===>\n', 'host_port set to: ', host_port, '\n<===')
+print('===>\n', 'host_port set to: ', host_port, '\n<===\n')
 
 #Build docker image:
+print('Building docker image...\nIt should take only few seconds, so please be patient...')
 try:
     client.images.build(path = path_to_docker, tag = image_name)
-except as e:
+except Exception as e:
     print('ERROR! The image was not built successfully!')
     print(e)
-	exit(1)
+    exit(1)
 else:
-    print('SUCCESS! The image was built successfully!')
+    print('SUCCESS! The docker image was built successfully!\n')
 
-#Run container
+#Run container:
+print('Running docker conteiner...\nIt should take about one second, so please be patient even more...')
 try:
     container = client.containers.run(image_name, ports = {container_port: host_port}, name = container_name, detach=True)
-except as e:
+except Exception as e:
     print('ERROR! The container was not run successfully!')
     print(e)
     exit(1)
+else:
+    print('SUCCESS! The docker container was run successfully!\n')
+    print('Container name: ', container.name)
+    print('Container image: ', container.image)
+    print('Container labels: ', container.labels)
+    print('Container ID: ', container.id)
+    print('Container short_id: ', container.short_id)
+    print('Container status: ', container.status, '\n')
+
+#Create a crontab file for Watcher:
+print('Creating Watcher_' + container_name + ' crontab file and putting it to /etc/cron.d/')
+try:
+    local_path = os.path.dirname(os.path.abspath(__file__))
+    watcher_filename = '{}{}'.format('Watcher_', container_name)
+    watcher_filepath = '{}/{}{}'.format(local_path, 'Watcher_', container_name)
+    logfile = '{}/{}{}{}'.format(local_path, 'Watcher_', container_name, '.log')
+    file = open(watcher_filepath, 'w')
+    file.write('{}{}{}{}{}{}{}{}{}'.format('*/1 * * * * root ', local_path, '/Watcher.sh ', container_name, ' >> ', local_path, '/Watcher_', container_name, '.log 2>&1\n'))
+    file.close()
+    copyfile(watcher_filepath, '{}{}'.format('/etc/cron.d/', watcher_filename))
+except Exception as e:
+    print('ERROR! Failed create Watcher!')
+    print(e)
+    exit(1)
+else:
+    print('SUCCESS! The Watcher was created successfully!')
+    print('You can monitor the conteiner status in ' + logfile + '/n')
 
